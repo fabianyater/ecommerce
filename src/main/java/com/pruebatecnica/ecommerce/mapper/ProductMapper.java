550df099d03bcc27aa762f55c70616068b7eaa89
Fabian Yate <fabianyate31@gmail.com>
2025-01-08 22:14:00 -0500

    Add ProductMapper interface for entity and DTO conversions
    - Implemented MapStruct-based mapper for converting between Product, ProductRequest, and ProductDocument.
    - Configured unmapped properties to be ignored to simplify mapping logic.

---
+++ src/main/java/com/pruebatecnica/ecommerce/mapper/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.pruebatecnica.ecommerce.mapper;
+
+import com.pruebatecnica.ecommerce.dto.request.ProductRequest;
+import com.pruebatecnica.ecommerce.entity.ProductDocument;
+import com.pruebatecnica.ecommerce.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface ProductMapper {
+    ProductDocument toDocument(Product product);
+    Product toProduct(ProductRequest productRequest);
+}
