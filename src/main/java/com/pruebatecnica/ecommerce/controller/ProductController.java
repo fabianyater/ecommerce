46cbe574970fe75a6b9cc0f0920d9636de7d7a32
Fabian Yate <fabianyate31@gmail.com>
2025-01-09 19:00:45 -0500

    Add swagger OpenAPI definition for documentation

--- src/main/java/com/pruebatecnica/ecommerce/controller/ProductController.java
+++ src/main/java/com/pruebatecnica/ecommerce/controller/ProductController.java
@@ -7,10 +7,19 @@ import com.pruebatecnica.ecommerce.dto.response.ProductResponse;
 import com.pruebatecnica.ecommerce.mapper.ProductMapper;
 import com.pruebatecnica.ecommerce.model.Product;
 import com.pruebatecnica.ecommerce.service.IProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,12 +36,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "Product", description = "Product API")
 public class ProductController {
     private final IProductService productService;
     private final ProductMapper productMapper;
 
+    @Operation(
+            summary = "Create a new product",
+            description = "This method allows the creation of a new product in the system."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Article created", content = @io.swagger.v3.oas.annotations.media.Content),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorResponse.class),
+                    examples = {
+                            @ExampleObject(name = "nameLength", value = "Name must be between 3 and 50 characters"),
+                            @ExampleObject(name = "priceValue", value = "Price must be greater than 0"),
+                            @ExampleObject(name = "stockValue", value = "Stock must be greater than or equal to 0")
+                    }
+            )),
+
+    })
     @PostMapping("/")
-    public ResponseEntity<Void> createProduct(@Valid @RequestBody ProductRequest productRequest) {
+    public ResponseEntity<Void> createProduct(
+            @Parameter(description = "Product data", required = true)
+            @Valid @RequestBody ProductRequest productRequest) {
         var product = productMapper.toProduct(productRequest);
 
         productService.createNewProduct(product);
@@ -41,10 +70,24 @@ public class ProductController {
         return ResponseEntity.created(location).build();
     }
 
+
+    @Operation(
+            summary = "Get all products",
+            description = "This method returns a list of all products in the system."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of products", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = PagedResponse.class)
+            ))
+    })
     @GetMapping("/")
     public ResponseEntity<PagedResponse<ProductListResponse>> getAllProductsPaged(
+            @Parameter(description = "Page number", required = true)
             @RequestParam int page,
+            @Parameter(description = "Page size", required = true)
             @RequestParam int size,
+            @Parameter(description = "Sort direction", required = true)
             @RequestParam String direction) {
         Page<Product> products = productService.getAllProductsPaged(page, size, direction);
 
@@ -71,16 +114,41 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "Get product by ID",
+            description = "This method returns a product by its ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product found", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductResponse.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Product not found", content = @Content)
+    })
     @GetMapping("/{productId}")
-    public ResponseEntity<ProductResponse> getProductById(@PathVariable String productId) {
+    public ResponseEntity<ProductResponse> getProductById(
+            @Parameter(description = "Product ID", required = true)
+            @PathVariable String productId) {
         var product = productService.getProductById(productId);
         ProductResponse productResponse = productMapper.toProductResponse(product);
 
         return ResponseEntity.ok(productResponse);
     }
 
+    @Operation(
+            summary = "Update product",
+            description = "This method allows the update of a product in the system."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Product updated", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product not found", content = @Content)
+    })
     @PutMapping("/{productId}")
-    public ResponseEntity<Void> updateProduct(@PathVariable String productId, @Valid @RequestBody ProductRequest productRequest) {
+    public ResponseEntity<Void> updateProduct(
+            @Parameter(description = "Product ID", required = true)
+            @PathVariable String productId,
+            @Parameter(description = "Product data", required = true)
+            @Valid @RequestBody ProductRequest productRequest) {
         var product = productMapper.toProduct(productRequest);
         product.setProductId(productId);
 
@@ -89,8 +157,18 @@ public class ProductController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "Delete product",
+            description = "This method allows the deletion of a product in the system."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Product deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Product not found", content = @Content)
+    })
     @DeleteMapping("/{productId}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable String productId) {
+    public ResponseEntity<Void> deleteProduct(
+            @Parameter(description = "Product ID", required = true)
+            @PathVariable String productId) {
         productService.deleteProduct(productId);
 
         return ResponseEntity.noContent().build();
